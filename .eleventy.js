96da2148b22fe2adee897d9fce15a999493cd20e
Adam Chapman <adam@steeplinedesign.com>
2020-11-22 20:30:49 -0700

    New static site scaffold

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets");
+    return {
+      dir: {
+        input: "src/site",
+        includes: "_includes",
+        output: "_site",
+      },
+    };
+  };
